d7d8caa0d5019562b6caa8b805be35fc1b717412
Jogendar Bairagi <53000049+Jogendar-Bairagi@users.noreply.github.com>
2026-03-01 12:50:47 +0530

    Refactor Firebase config to use environment variables
    Updated Firebase configuration to use environment variables for sensitive information.

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,14 @@ import { getAuth } from "firebase/auth";
 import { data } from "react-router-dom";
 import { getDatabase } from "firebase/database";
 const firebaseConfig = {
-  apiKey: "AIzaSyAsoiPdIrDA3M-xVpUjxPdoKed4BiaePF8",
-  authDomain: "bus-tracker-229b6.firebaseapp.com",
-  projectId: "bus-tracker-229b6",
-  storageBucket: "bus-tracker-229b6.firebasestorage.app",
-  messagingSenderId: "1000838322125",
-  appId: "1:1000838322125:web:00336b3b5dd6926720d684",
-  databaseURL: "https://bus-tracker-229b6-default-rtdb.firebaseio.com",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL, 
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 export const db = getDatabase(initializeApp(firebaseConfig));
-// Initialize Firebase
+
 export const app = initializeApp(firebaseConfig);
